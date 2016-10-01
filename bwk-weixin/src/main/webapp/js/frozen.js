7e42536cf90061001e6246e395141ec66e62111e
caihao <caihao@megvii.com>
2016-10-01 10:32:05 +0800

    update

--- bwk-weixin/src/main/webapp/js/frozen.js
+++ bwk-weixin/src/main/webapp/js/frozen.js
@@ -131,3 +131,40 @@ BWK.Utils = {};
 BWK.Utils.dateformate = function(date){
 	return  date.toLocaleDateString().replace(/\//g, "-");   
 }
+//"yyyy-MM-dd hh:mm:ss.S" ==> 2006-07-02 08:09:04.423   
+//"yyyy-M-d h:m:s.S"      ==> 2006-7-2 8:9:4.18   
+BWK.Utils.dateformateLocal = function(date,fmt){
+	  var o = { 
+	    "M+" : date.getMonth()+1,                 //月份 
+	    "d+" : date.getDate(),                    //日 
+	    "h+" : date.getHours(),                   //小时 
+	    "m+" : date.getMinutes(),                 //分 
+	    "s+" : date.getSeconds(),                 //秒 
+	    "q+" : Math.floor((date.getMonth()+3)/3), //季度 
+	    "S"  : date.getMilliseconds()             //毫秒 
+	  }; 
+	  if(/(y+)/.test(fmt)) 
+	    fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+	  for(var k in o) 
+	    if(new RegExp("("+ k +")").test(fmt)) 
+	  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length))); 
+	  return fmt; 
+}
+
+
+BWK.Utils.Tips = function(content){
+	var str = '<div class="ui-poptips ui-poptips-success">\
+	    <div class="ui-poptips-cnt" style="height:auto;"><i></i><span class="ui-poptips-text">'+content+'</span></div>\
+	    </div>';  
+	$('body').append(str);
+	setTimeout(function(){$(".ui-poptips .ui-poptips-cnt").height(0);$(".ui-poptips").remove();},3000);
+}
+BWK.Utils.dialog = function(){
+	
+}
+BWK.Page = function(){
+	
+}
+BWK.Weixin = function(){
+	
+}
